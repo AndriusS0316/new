f3ea42f51010d9f948f6f9287657e548da86dc58
Andrius <andriuss0316@gmail.com>
2021-08-11 14:56:32 +0300

    20210811

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+
+let sk = 20485; // is skaiciaus pavertimas i stringa
+
+while (sk > 0) {
+    let skaitmuo = skaitmuo: Number
+    console.log(skaitmuo);
+    sk = (sk - sk % 10) / 10
+}
+
+let tekstas = "20485"; // is stringo pavertimas i skaiciu
+
+
+let sk = tekstas + 1;
